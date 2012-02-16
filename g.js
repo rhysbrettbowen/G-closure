8d6cc7888eaa7ad14627b4d2a2bdf5321f182de5
Rhsy <rhysbrettbowen@hotmail.com>
2012-02-15 18:40:02 -0800

    updates

--- g.js
+++ g.js
@@ -40,8 +40,12 @@ G = function(input, mod) {
 };
 
 /**
+ * takes a string like 'tagName[ .className]', '.className' or '#elementId'
+ * mod is the element to search from
+ *
  * @param {string} input
- * @param {Element|Node=} mod
+ * @param {Element|Node=} mod 
+ * @return {Array.<Element|Node>}
  */
 G.elsBySelector = function(input, mod) {
     if(input.charAt(0) == '.') {
@@ -57,34 +61,70 @@ G.elsBySelector = function(input, mod) {
  */
 G.prototype = {
     // Array Functions
+    /**
+     * @param {Function} fn
+     * @param {Object} handler
+     * @return {goog.array.ArrayLike}
+     */
     each: function(fn, handler) {
         goog.array.forEach(this, fn, handler);
         return this;
     },
+    /**
+     * @param {Function} fn
+     * @param {Object} handler
+     * @return {goog.array.ArrayLike}
+     */
     filter: function(fn, handler) {
         return G(goog.array.filter(this, fn, handler));
     },
+    /**
+     * @param {Function} fn
+     * @param {Object} handler
+     * @return {goog.array.ArrayLike}
+     */
     map: function(fn, handler) {
         return G(goog.array.map(this, fn, handler));
     },
+    /**
+     * @param {number} in
+     * @return {?Object}
+     */
     get: function(ind) {
         if(ind < 0)
             ind = this.length + ind;
         return this[ind || 0];
     },
+    /**
+     * @return {?Object}
+     */
     first: function(){
         return this.get(0);
     },
+    /**
+     * @return {?Object}
+     */
     last: function(){
         return this.get(-1);
     },
+    /**
+     * @return {number}
+     */
     size: function() {
         return this.length;
     },
+    /**
+     * @param {goog.array.ArrayLike} arr
+     * @return {goog.array.ArrayLike}
+     */
     add: function(arr) {
         return G(goog.array.concat(this, arr));
     },
     // DOM functions
+    /**
+     * @param {string} selector
+     * @return {goog.array.ArrayLike}
+     */
     find: function(selector) {
         var ret = [];
         this.each(function(el) {
@@ -96,12 +136,22 @@ G.prototype = {
         });
         return G(ret);
     },
+    /**
+     * @return {goog.array.ArrayLike}
+     */
     next: function() {
         return this.map(function(el) {return el.nextSibling;});
     },
+    /**
+     * @return {goog.array.ArrayLike}
+     */
     prev: function() {
         return this.map(function(el) {return el.previousSibling;});
     },
+    /**
+     * @param {Element|Node|Function|string=} input
+     * @return {goog.array.ArrayLike}
+     */
     html: function(input) {
         if(!input)
             return this.get().innerHTML
@@ -113,6 +163,10 @@ G.prototype = {
             this.each(function(el) {el.innerHTML = input});
         return this;
     },
+    /**
+     * @param {Element|Node|Function|string=} input
+     * @return {goog.array.ArrayLike}
+     */
     text: function(input) {
         if(!input)
             return goog.dom.getTextContent(this.get());
@@ -123,14 +177,30 @@ G.prototype = {
         return this;
     },
     // Events
-    click: function(fn, handler, eventObject) {
+    /**
+     * @param {string} eventType
+     * @param {Function} fn
+     * @param {Object=} handler
+     * @param {goog.events=} eventObject 
+     * @return {goog.array.ArrayLike}
+     */
+    bind: function(eventType, fn, handler, eventObject) {
         this.each(function(el) {
             if(eventObject)
-                eventObject.listen(el, goog.events.EventType.CLICK, fn, false, (handler || el));
+                eventObject.listen(el, eventType, fn, false, (handler || el));
             else
-                goog.events.listen(el, goog.events.EventType.CLICK, fn, false, (handler || el));
+                goog.events.listen(el, eventType, fn, false, (handler || el));
         });
         return this;
+    },
+    /**
+     * @param {Function} fn
+     * @param {Object=} handler
+     * @param {goog.events=} eventObject 
+     * @return {goog.array.ArrayLike}
+     */
+    click: function(fn, handler, eventObject) {
+        return this.bind(goog.events.EventType.CLICK);
     }
 }
 
